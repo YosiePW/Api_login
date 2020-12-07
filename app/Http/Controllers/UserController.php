16619f51c70a54cd7beba2991860b7d35f21f8eb
YosiePW <yosieka_putri_27rpl@student.smktelkom-mlg.sch.id>
2020-12-07 21:57:49 +0700

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+
+class UserController extends Controller
+{
+    public function login(Request $request){
+		$credentials = $request->only('email', 'password');
+
+		try {
+			if(!$token = JWTAuth::attempt($credentials)){
+				return response()->json([
+						'logged' 	=>  false,
+						'message' 	=> 'Invalid email and password'
+					]);
+			}
+		} catch(JWTException $e){
+			return response()->json([
+						'logged' 	=> false,
+						'message' 	=> 'Generate Token Failed'
+					]);
+		}
+		return response()->json([
+					"logged"    => true,
+                    "token"     => $token,
+                    "message" 	=> 'Login berhasil'
+		]);
+	}
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|same:password_confirmation',
+            'password_confirmation' => 'required|string|min:6',
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        $user = User::create([
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'password' => Hash::make($request->get('password')),
+            'password_confirmation' => Hash::make($request->get('password_confirmation')),
+        ]);
+
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json(compact('user','token'),201);
+    }
+
+    public function getAuthenticatedUser()
+    {
+        try {
+
+            if (! $user = JWTAuth::parseToken()->authenticate()) {
+                return response()->json(['user_not_found'], 404);
+            }
+
+        } catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+
+            return response()->json(['token_expired'], $e->getStatusCode());
+
+        } catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+
+            return response()->json(['token_invalid'], $e->getStatusCode());
+
+        } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
+
+            return response()->json(['token_absent'], $e->getStatusCode());
+
+        }
+
+        return response()->json(compact('user'));
+    }
+
+    public function LoginCheck(){
+		try {
+			if(!$user = JWTAuth::parseToken()->authenticate()){
+				return response()->json([
+						'auth' 		=> false,
+						'message'	=> 'Invalid token'
+					]);
+			}
+		} catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e){
+			return response()->json([
+						'auth' 		=> false,
+						'message'	=> 'Token expired'
+					], $e->getStatusCode());
+		} catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e){
+			return response()->json([
+						'auth' 		=> false,
+						'message'	=> 'Invalid token'
+					], $e->getStatusCode());
+		} catch (Tymon\JWTAuth\Exceptions\JWTException $e){
+			return response()->json([
+						'auth' 		=> false,
+						'message'	=> 'Token absent'
+					], $e->getStatusCode());
+		}
+
+		 return response()->json([
+		 		"auth"      => true,
+                "user"    => $user
+		 ], 201);
+	}
+}
---
+++ app/User.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class User extends Authenticatable implements JWTSubject
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password', 'password_confirmation'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'password_confirmation','remember_token',
+    ];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+}
